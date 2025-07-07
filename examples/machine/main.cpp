8ee84df9e9161961f714a5db044d36140bf40d90
sivansh11 <sivanshgupta2002@gmail.com>
2025-07-07 09:04:05 +0530

    added comments

--- examples/machine/main.cpp
+++ examples/machine/main.cpp
@@ -8,10 +8,12 @@ static bool running = true;
 int main(int argc, char** argv) {
   if (argc < 2) throw std::runtime_error("Error: [dawn] [elf]");
   dawn::machine_t machine{1024 * 1024 * 1, 1024};
+  // newlib exit
   machine.set_syscall(93, [](dawn::machine_t& machine) {
     running = false;
     exit(machine._registers[10]);
   });
+  // my_print
   machine.set_syscall(1000, [](dawn::machine_t& machine) {
     uint64_t i = 0;
     while (char ch = machine._memory.load<8>(machine._registers[10] + i++)) {
