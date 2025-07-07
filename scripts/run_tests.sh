cbab977e90ac7c510784762bede40c731bf6e894
sivansh11 <sivanshgupta2002@gmail.com>
2025-07-07 07:43:31 +0530

    updated riscv-tests path

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d "tests" ]; then
-  git clone --recursive https://github.com/riscv-software-src/riscv-tests.git ./tests
+  git clone --recursive https://github.com/riscv-software-src/riscv-tests.git ./tests/riscv-tests
   if [ -z "$RISCV" ]; then
     echo "Error: RISCV environment variable is not set."
     exit 1
@@ -11,10 +11,10 @@ if [ ! -d "tests" ]; then
     echo "Error: riscv64-unknown-elf-gcc could not be found."
     exit 1
   fi
-  cd tests
+  cd tests/riscv-tests
   ./configure --prefix=$RISCV/target
   make
-  cd ..
+  cd ../..
 fi
 
 if [ "$#" -ne 2 ]; then
@@ -30,7 +30,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-find tests/isa -type f -name "${TEST_PATTERN}*" -executable -not -name "*.dump" | while read test_file; do
+find tests/riscv-tests/isa -type f -name "${TEST_PATTERN}*" -executable -not -name "*.dump" | while read test_file; do
   echo "--- Running test: $test_file ---"
   $BINARY_PATH "$test_file"
   if [ $? -eq 0 ]; then
