ef7b5557b98862804bf630ba2540d6f75d44d9b0
sivansh11 <sivanshgupta2002@gmail.com>
2025-07-13 13:12:18 +0530

    updated debug

--- examples/debug/main.cpp
+++ examples/debug/main.cpp
@@ -9,49 +9,57 @@
 
 #include "machine.hpp"
 
+static bool running = true;
+
 int main(int argc, char** argv) {
   if (argc < 2) throw std::runtime_error("Error: [dawn] [elf]");
   dawn::machine_t machine{1024 * 1024 * 1, 1024};
   // newlib exit
-  machine.set_syscall(
-      93, [](dawn::machine_t& machine) { exit(machine._registers[10]); });
+  machine.set_syscall(93, [](dawn::machine_t& machine) {
+    running = false;
+    exit(machine._registers[10]);
+  });
   // newlib brk
   machine.set_syscall(214, [](dawn::machine_t& machine) {
     uint64_t new_end = machine._registers[10];
     std::cout << "new_end: " << std::hex << std::setw(8) << new_end
               << " _heap_address: " << std::setw(8) << machine._heap_address
-              << std::dec << '\n';
+              << " program_counter: " << std::setw(8)
+              << machine._program_counter << std::dec << '\n';
     if (new_end < machine._heap_address) {
       new_end = machine._heap_address;
     }
     machine._registers[10] = new_end;
   });
-  // newlib
-  machine.set_syscall(
-      57, [](dawn::machine_t& machine) { machine._registers[10] = 0; });
   // newlib write
   machine.set_syscall(64, [](dawn::machine_t& machine) {
     int      vfd     = machine._registers[10];
     uint64_t address = machine._registers[11];
     size_t   len     = machine._registers[12];
     if (vfd == 1 || vfd == 2) {
+      for (uint64_t i = address; i < address + len; i++) {
+        std::cout << (char)machine._memory.load<8>(i);
+      }
       machine._registers[10] = len;
     } else {
       machine._registers[10] = -9;
     }
   });
+  // newlib
+  machine.set_syscall(
+      57, [](dawn::machine_t& machine) { machine._registers[10] = 0; });
   // newlib fstat
   machine.set_syscall(
       80, [](dawn::machine_t& machine) { machine._registers[10] = -38; });
   // my_print
   machine.set_syscall(1000, [](dawn::machine_t& machine) {
     uint64_t i = 0;
+    std::cout << "[ERROR]: ";
     while (char ch = machine._memory.load<8>(machine._registers[10] + i++)) {
       std::cout << ch;
     }
   });
   machine.load_elf_and_set_program_counter(argv[1]);
-  bool     running = true;
   uint32_t address = std::numeric_limits<uint32_t>::max();
   if (argc == 3) {
     address = static_cast<uint32_t>(std::stoul(argv[2], nullptr, 16));
