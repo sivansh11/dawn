af881cdb1259ae9efd1242a4a5456f25ba1a1f44
sivansh11 <sivanshgupta2002@gmail.com>
2026-01-10 18:09:09 +0530

    added machine_external_interrupt

--- includes/dawn/dawn.hpp
+++ includes/dawn/dawn.hpp
@@ -58,7 +58,8 @@ enum class exception_code_t : uint64_t {
   e_ecall_s_mode                   = 9,
   e_ecall_m_mode                   = 11,
 
-  e_machine_timer_interrupt = 7 | (1ull << 63),
+  e_machine_timer_interrupt    = 7 | (1ull << 63),
+  e_machine_external_interrupt = 11 | (1ull << 63),
 };
 
 constexpr uint64_t MHARDID = 0xf14;
