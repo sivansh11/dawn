a199f50a5b5102b64d5babea5e66b6a1701d7b75
sivansh11 <sivanshgupta2002@gmail.com>
2026-02-19 13:49:38 +0530

    updated helper script

--- scripts/run_linux.sh
+++ scripts/run_linux.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-cmake --build build
+cmake --build build --target linux
 if [ $? -ne 0 ]; then
   echo "Build failed"
   exit
 fi
-# riscv64-unknown-elf-objdump -M numeric -M no-aliases -d ../linux-6.18/vmlinux --adjust-vma=0x0 > vmlinux_dump 
-
-cd examples/linux
-dtc -I dts -O dtb -o dt.dtb dt.dts
-cd ../../
-
-# ./build/examples/linux/linux ../linux-6.18/arch/riscv/boot/Image examples/linux/dt.dtb
-./build/examples/linux/linux ../buildroot/output/images/Image examples/linux/dt.dtb
+./build/examples/linux/linux ./examples/linux/Image ./examples/linux/rootfs.cpio 
